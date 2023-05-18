9f58b1ceedabb1688c90408247a2aaca3568b436
CarlosEduardoGit <cadudjn1@gmail.com>
2023-05-18 16:51:03 -0300

    Criação do perfil dev + Configurações do Banco de Dados Postgres + Criação do Método GetById

---
+++ src/main/java/com/app/bookstore/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.app.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.app.bookstore.service.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaBaseDeDados() {
+		this.dbService.instanciaBaseDeDados();
+	}
+}
