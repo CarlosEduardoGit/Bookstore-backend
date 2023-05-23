1b827106fd86e41b79dcbee4f22577ec5cf771f8
CarlosEduardoGit <cadudjn1@gmail.com>
2023-05-22 21:54:16 -0300

    Criação dos Métodos PUT e PATCH do Objeto Livro

--- src/main/java/com/app/bookstore/service/LivroService.java
+++ src/main/java/com/app/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.bookstore.dtos.LivroDTO;
 import com.app.bookstore.model.Categoria;
 import com.app.bookstore.model.Livro;
 import com.app.bookstore.repositories.CategoriaRepository;
@@ -33,4 +34,20 @@ public class LivroService {
 		categoriaService.findById(id_cat);
 		return livroRepository.findAllByCategoria(id_cat);
 	}
+
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return livroRepository.save(newObj);
+	}
+
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
+	}
+	
+	
 }
